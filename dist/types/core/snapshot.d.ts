9a74b0307e20dcfdeb08d199ad424b5243437d96
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-14 12:52:17 -0500

    @hotwired/turbo/8f95b10 Read turbo-frame target from Submitter
    https://github.com/hotwired/turbo/tree/8f95b10888b981b7d1d96a8cf9185da2834248de

---
+++ dist/types/core/snapshot.d.ts
@@ -0,0 +1,12 @@
+export declare class Snapshot<E extends Element = Element> {
+    readonly element: E;
+    constructor(element: E);
+    get children(): Element[];
+    hasAnchor(anchor: string): boolean;
+    getElementForAnchor(anchor: string): Element | null;
+    get isConnected(): boolean;
+    get firstAutofocusableElement(): Element | null;
+    get permanentElements(): Element[];
+    getPermanentElementById(id: string): Element | null;
+    getPermanentElementsPresentInSnapshot(snapshot: Snapshot): Element[];
+}
