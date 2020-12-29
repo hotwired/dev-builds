96e9c9c7bada1be00557f788ad11f7a0c2b685cf
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-28 21:50:17 -0500

    @hotwired/turbo/01406de Restrict Accept: turbo-stream to Form Submissions
    https://github.com/hotwired/turbo/tree/01406de7788e86e952982d85a576a3477d6006f5

---
+++ dist/types/core/streams/stream_message.d.ts
@@ -0,0 +1,10 @@
+import { StreamElement } from "../../elements/stream_element";
+export declare const StreamMessageContentType = "text/vnd.turbo-stream.html";
+export declare class StreamMessage {
+    readonly templateElement: HTMLTemplateElement;
+    static wrap(message: StreamMessage | string): StreamMessage;
+    constructor(html: string);
+    get fragment(): DocumentFragment;
+    get foreignElements(): StreamElement[];
+    get templateChildren(): Element[];
+}
