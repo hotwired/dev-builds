1f9f5e57f5a3039abaabc74aafaa12724c7fa460
Sean Doyle <sean.p.doyle24@gmail.com>
2021-01-29 09:05:05 -0500

    @hotwired/turbo/ca350d9 Restore correct Accept header for form submissions
    https://github.com/hotwired/turbo/tree/ca350d99be77be436b6f4df6e461038dda7d19f4

---
+++ dist/types/core/url.d.ts
@@ -0,0 +1,7 @@
+export declare type Locatable = URL | string;
+export declare function expandURL(locatable: Locatable): URL;
+export declare function getAnchor(url: URL): string;
+export declare function getExtension(url: URL): string;
+export declare function isHTML(url: URL): boolean;
+export declare function isPrefixedBy(baseURL: URL, url: URL): boolean;
+export declare function toCacheKey(url: URL): string;
