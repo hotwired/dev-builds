a5d0bf6e0defd4fc1e8ecf0a2cc2ba2b27aaece0
Sam Stephenson <sam@hey.com>
2021-02-23 14:28:06 -0600

    @hotwired/turbo/dd0aaf8 Prevent frames from loading in cached snapshots
    https://github.com/hotwired/turbo/tree/dd0aaf830f1ea9f5b4d710fbd3c2010dfe7583c8

---
+++ dist/types/tests/functional/stream_tests.d.ts
@@ -0,0 +1,5 @@
+import { FunctionalTestCase } from "../helpers/functional_test_case";
+export declare class StreamTests extends FunctionalTestCase {
+    setup(): Promise<void>;
+    "test receiving a stream message"(): Promise<void>;
+}
