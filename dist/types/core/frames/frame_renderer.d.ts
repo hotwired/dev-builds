5afe99c94a0bb13f37a6c2b361a71da32f734408
Sam Stephenson <sam@hey.com>
2021-02-23 23:51:28 -0600

    @hotwired/turbo/1d52b23 Prevent frames from reloading after reconnecting
    https://github.com/hotwired/turbo/tree/1d52b23fd08154849f1f1255fcd55e751cf2ca10

---
+++ dist/types/core/frames/frame_renderer.d.ts
@@ -0,0 +1,8 @@
+import { FrameElement } from "../../elements/frame_element";
+import { Renderer } from "../renderer";
+export declare class FrameRenderer extends Renderer<FrameElement> {
+    get shouldRender(): boolean;
+    render(): Promise<void>;
+    loadFrameElement(): void;
+    scrollFrameIntoView(): boolean;
+}
