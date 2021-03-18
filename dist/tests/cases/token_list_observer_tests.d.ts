753601bab05af8597eda48c8a939e6958725482c
Sam Stephenson <sam@hey.com>
2021-03-18 17:52:44 -0500

    @stimulus/mutation-observers/b950116 Experiment with a new syntax for defining controller properties
    https://github.com/hotwired/stimulus/tree/b950116f28e1b327c1fa219a3c78467d423d09ae

---
+++ dist/tests/cases/token_list_observer_tests.d.ts
@@ -0,0 +1,18 @@
+import { Token, TokenListObserver, TokenListObserverDelegate } from "../../token_list_observer";
+import { ObserverTestCase } from "../observer_test_case";
+export default class TokenListObserverTests extends ObserverTestCase implements TokenListObserverDelegate {
+    attributeName: string;
+    fixtureHTML: string;
+    observer: TokenListObserver;
+    "test tokenMatched"(): Promise<void>;
+    "test adding a token to the right"(): Promise<void>;
+    "test inserting a token in the middle"(): Promise<void>;
+    "test removing the leftmost token"(): Promise<void>;
+    "test removing the rightmost token"(): Promise<void>;
+    "test removing the only token"(): Promise<void>;
+    get element(): Element;
+    set tokenString(value: string);
+    tokenMatched(token: Token): void;
+    tokenUnmatched(token: Token): void;
+}
+//# sourceMappingURL=token_list_observer_tests.d.ts.map
