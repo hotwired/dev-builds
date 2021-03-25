dc6e14ad6db3755a2a8c4612d1f56ede37e2b0ec
Sam Stephenson <sam@hey.com>
2021-03-25 12:15:22 -0500

    @stimulus/core/7363d5f Remove typedoc
    https://github.com/hotwired/stimulus/tree/7363d5fede3f7a7e00caf8dfbce2e7aedf662de0

---
+++ dist/tests/cases/application_test_case.d.ts
@@ -0,0 +1,10 @@
+import { Application } from "../../application";
+import { DOMTestCase } from "@stimulus/test";
+import { Schema } from "../../schema";
+export declare class ApplicationTestCase extends DOMTestCase {
+    schema: Schema;
+    application: Application;
+    runTest(testName: string): Promise<void>;
+    setupApplication(): void;
+}
+//# sourceMappingURL=application_test_case.d.ts.map
---
+++ dist/value_properties.d.ts
@@ -0,0 +1,19 @@
+import { Constructor } from "./constructor";
+export declare function ValuePropertiesBlessing<T>(constructor: Constructor<T>): PropertyDescriptorMap;
+export declare function propertiesForValueDefinitionPair<T>(valueDefinitionPair: ValueDefinitionPair): PropertyDescriptorMap;
+export declare type ValueDescriptor = {
+    type: ValueType;
+    key: string;
+    name: string;
+    defaultValue: any;
+};
+export declare type ValueDescriptorMap = {
+    [attributeName: string]: ValueDescriptor;
+};
+export declare type ValueDefinitionMap = {
+    [token: string]: ValueTypeConstant;
+};
+export declare type ValueDefinitionPair = [string, ValueTypeConstant];
+export declare type ValueTypeConstant = typeof Array | typeof Boolean | typeof Number | typeof Object | typeof String;
+export declare type ValueType = "array" | "boolean" | "number" | "object" | "string";
+//# sourceMappingURL=value_properties.d.ts.map
