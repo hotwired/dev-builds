5ee57bfce0e23e7de60b2f007bb91e9b588b46cd
GitHub <noreply@github.com>
2021-06-08 10:59:03 +0200

    @hotwired/turbo/430cd50 Merge pull request #280 from excid3/set-window-var
    https://github.com/hotwired/turbo/tree/430cd50083bea8814cfd2e9b83e1a18c3b236b94

---
+++ dist/types/tests/unit/stream_element_tests.d.ts
@@ -0,0 +1,11 @@
+import { DOMTestCase } from "../helpers/dom_test_case";
+export declare class StreamElementTests extends DOMTestCase {
+    beforeTest(): Promise<void>;
+    "test action=append"(): Promise<void>;
+    "test action=append with children ID already present in target"(): Promise<void>;
+    "test action=prepend"(): Promise<void>;
+    "test action=prepend with children ID already present in target"(): Promise<void>;
+    "test action=remove"(): Promise<void>;
+    "test action=replace"(): Promise<void>;
+    "test action=update"(): Promise<void>;
+}
