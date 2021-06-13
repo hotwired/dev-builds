b646c146bd34e04995c46fa0133834aaebd53636
David Heinemeier Hansson <david@loudthinking.com>
2021-06-13 10:37:38 +0200

    @stimulus/core/334f417 Recommend using window.Stimulus instead of application for better debugging
    https://github.com/hotwired/stimulus/tree/334f4177d846565960f95d1c80cb5b8b11341c10

---
+++ dist/class_properties.d.ts
@@ -0,0 +1,3 @@
+import { Constructor } from "./constructor";
+export declare function ClassPropertiesBlessing<T>(constructor: Constructor<T>): PropertyDescriptorMap;
+//# sourceMappingURL=class_properties.d.ts.map
