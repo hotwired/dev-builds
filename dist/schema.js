dc144371f96045a70c0782e0271cad243097c07c
GitHub <noreply@github.com>
2021-07-02 06:48:22 -0700

    @stimulus/core/cc97aa7 Merge pull request #417 from Sub-Xaero/fix/ci-ie11
    https://github.com/hotwired/stimulus/tree/cc97aa7dea528cf05156cb32beb9d68d6a7e8669

---
+++ dist/schema.js
@@ -0,0 +1,7 @@
+export var defaultSchema = {
+    controllerAttribute: "data-controller",
+    actionAttribute: "data-action",
+    targetAttribute: "data-target",
+    targetAttributeForScope: function (identifier) { return "data-" + identifier + "-target"; }
+};
+//# sourceMappingURL=schema.js.map
---
+++ dist/tests/cases/log_controller_test_case.js
@@ -0,0 +1,119 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+import { ControllerTestCase } from "./controller_test_case";
+import { LogController } from "../controllers/log_controller";
+var LogControllerTestCase = /** @class */ (function (_super) {
+    __extends(LogControllerTestCase, _super);
+    function LogControllerTestCase() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    LogControllerTestCase.prototype.setup = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        this.controllerConstructor.actionLog = [];
+                        return [4 /*yield*/, _super.prototype.setup.call(this)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    LogControllerTestCase.prototype.assertActions = function () {
+        var _this = this;
+        var actions = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            actions[_i] = arguments[_i];
+        }
+        this.assert.equal(this.actionLog.length, actions.length);
+        actions.forEach(function (expected, index) {
+            var keys = Object.keys(expected);
+            var actual = slice(_this.actionLog[index] || {}, keys);
+            var result = keys.every(function (key) { return deepEqual(expected[key], actual[key]); });
+            _this.assert.pushResult({ result: result, actual: actual, expected: expected, message: "" });
+        });
+    };
+    LogControllerTestCase.prototype.assertNoActions = function () {
+        this.assert.equal(this.actionLog.length, 0);
+    };
+    Object.defineProperty(LogControllerTestCase.prototype, "actionLog", {
+        get: function () {
+            return this.controllerConstructor.actionLog;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return LogControllerTestCase;
+}(ControllerTestCase(LogController)));
+export { LogControllerTestCase };
+function slice(object, keys) {
+    return keys.reduce(function (result, key) { return (result[key] = object[key], result); }, {});
+}
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) {
+        return true;
+    }
+    else {
+        if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+            return false;
+        }
+        for (var prop in obj1) {
+            if (!deepEqual(obj1[prop], obj2[prop])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//# sourceMappingURL=log_controller_test_case.js.map
