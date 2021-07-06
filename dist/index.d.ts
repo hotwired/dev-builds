0e3bbfc612056fdc83d99d940fa7cb88d0553d51
GitHub <noreply@github.com>
2021-07-06 10:43:23 -0700

    @stimulus/core/e4d51ba Merge pull request #419 from dancallaghan/default-value-fix
    https://github.com/hotwired/stimulus/tree/e4d51ba8134c3c25135c877883de7b89128248d6

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export { ActionEvent } from "./action_event";
+export { Application } from "./application";
+export { Context } from "./context";
+export { Controller, ControllerConstructor } from "./controller";
+export { Definition } from "./definition";
+export { Schema, defaultSchema } from "./schema";
+//# sourceMappingURL=index.d.ts.map
