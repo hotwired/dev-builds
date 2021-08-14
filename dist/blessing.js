5881e1ad2938268e73ea549acf58d018e280b2a1
GitHub <noreply@github.com>
2021-08-14 11:39:00 +0200

    @stimulus/core/720b35d Merge pull request #427 from hotwired/dependabot/npm_and_yarn/path-parse-1.0.7
    https://github.com/hotwired/stimulus/tree/720b35d37ae820260572859872a9a84e67f0459e

---
+++ dist/blessing.js
@@ -0,0 +1,104 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+import { readInheritableStaticArrayValues } from "./inheritable_statics";
+export function bless(constructor) {
+    return shadow(constructor, getBlessedProperties(constructor));
+}
+function shadow(constructor, properties) {
+    var shadowConstructor = extend(constructor);
+    var shadowProperties = getShadowProperties(constructor.prototype, properties);
+    Object.defineProperties(shadowConstructor.prototype, shadowProperties);
+    return shadowConstructor;
+}
+function getBlessedProperties(constructor) {
+    var blessings = readInheritableStaticArrayValues(constructor, "blessings");
+    return blessings.reduce(function (blessedProperties, blessing) {
+        var properties = blessing(constructor);
+        for (var key in properties) {
+            var descriptor = blessedProperties[key] || {};
+            blessedProperties[key] = Object.assign(descriptor, properties[key]);
+        }
+        return blessedProperties;
+    }, {});
+}
+function getShadowProperties(prototype, properties) {
+    return getOwnKeys(properties).reduce(function (shadowProperties, key) {
+        var _a;
+        var descriptor = getShadowedDescriptor(prototype, properties, key);
+        if (descriptor) {
+            Object.assign(shadowProperties, (_a = {}, _a[key] = descriptor, _a));
+        }
+        return shadowProperties;
+    }, {});
+}
+function getShadowedDescriptor(prototype, properties, key) {
+    var shadowingDescriptor = Object.getOwnPropertyDescriptor(prototype, key);
+    var shadowedByValue = shadowingDescriptor && "value" in shadowingDescriptor;
+    if (!shadowedByValue) {
+        var descriptor = Object.getOwnPropertyDescriptor(properties, key).value;
+        if (shadowingDescriptor) {
+            descriptor.get = shadowingDescriptor.get || descriptor.get;
+            descriptor.set = shadowingDescriptor.set || descriptor.set;
+        }
+        return descriptor;
+    }
+}
+var getOwnKeys = (function () {
+    if (typeof Object.getOwnPropertySymbols == "function") {
+        return function (object) { return __spreadArray(__spreadArray([], Object.getOwnPropertyNames(object)), Object.getOwnPropertySymbols(object)); };
+    }
+    else {
+        return Object.getOwnPropertyNames;
+    }
+})();
+var extend = (function () {
+    function extendWithReflect(constructor) {
+        function extended() {
+            var _newTarget = this && this instanceof extended ? this.constructor : void 0;
+            return Reflect.construct(constructor, arguments, _newTarget);
+        }
+        extended.prototype = Object.create(constructor.prototype, {
+            constructor: { value: extended }
+        });
+        Reflect.setPrototypeOf(extended, constructor);
+        return extended;
+    }
+    function testReflectExtension() {
+        var a = function () { this.a.call(this); };
+        var b = extendWithReflect(a);
+        b.prototype.a = function () { };
+        return new b;
+    }
+    try {
+        testReflectExtension();
+        return extendWithReflect;
+    }
+    catch (error) {
+        return function (constructor) { return /** @class */ (function (_super) {
+            __extends(extended, _super);
+            function extended() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            return extended;
+        }(constructor)); };
+    }
+})();
+//# sourceMappingURL=blessing.js.map
