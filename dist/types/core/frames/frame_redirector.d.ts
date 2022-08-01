d673891f174ee98e7d97953448a8f25423d7bbb8
GitHub <noreply@github.com>
2022-08-01 19:38:44 -0400

    @hotwired/turbo/60c6463 Return `Promise<void>` from `FrameElement.reload` (#661)
    https://github.com/hotwired/turbo/tree/60c646383b3c0355242d2e3c69f80742efb266be

---
+++ dist/types/core/frames/frame_redirector.d.ts
@@ -0,0 +1,19 @@
+import { FormSubmitObserver, FormSubmitObserverDelegate } from "../../observers/form_submit_observer";
+import { LinkClickObserver, LinkClickObserverDelegate } from "../../observers/link_click_observer";
+import { Session } from "../session";
+export declare class FrameRedirector implements LinkClickObserverDelegate, FormSubmitObserverDelegate {
+    readonly session: Session;
+    readonly element: Element;
+    readonly linkClickObserver: LinkClickObserver;
+    readonly formSubmitObserver: FormSubmitObserver;
+    constructor(session: Session, element: Element);
+    start(): void;
+    stop(): void;
+    willFollowLinkToLocation(element: Element): boolean;
+    followedLinkToLocation(element: Element, url: URL): void;
+    willSubmitForm(element: HTMLFormElement, submitter?: HTMLElement): boolean;
+    formSubmitted(element: HTMLFormElement, submitter?: HTMLElement): void;
+    private shouldSubmit;
+    private shouldRedirect;
+    private findFrameElement;
+}
