e175deedd7c8bb7c5778e4e50e1a6bbfa1262639
GitHub <noreply@github.com>
2023-08-25 09:56:43 +0100

    @hotwired/turbo/0826b81 Dispatch turbo:load after loading 404 response (#960)
    https://github.com/hotwired/turbo/tree/0826b8152c0e97f19d459c1a1c364fa89cc62829

---
+++ dist/types/core/drive/page_snapshot.d.ts
@@ -0,0 +1,18 @@
+import { Snapshot } from "../snapshot";
+import { HeadSnapshot } from "./head_snapshot";
+export declare class PageSnapshot extends Snapshot<HTMLBodyElement> {
+    static fromHTMLString(html?: string): PageSnapshot;
+    static fromElement(element: Element): PageSnapshot;
+    static fromDocument({ head, body }: Document): PageSnapshot;
+    readonly headSnapshot: HeadSnapshot;
+    constructor(element: HTMLBodyElement, headSnapshot: HeadSnapshot);
+    clone(): PageSnapshot;
+    get headElement(): HTMLHeadElement;
+    get rootLocation(): URL;
+    get cacheControlValue(): string | null;
+    get isPreviewable(): boolean;
+    get isCacheable(): boolean;
+    get isVisitable(): boolean;
+    get prefersViewTransitions(): boolean;
+    getSetting(name: string): string | null;
+}
