58229dcfbb06fa68ce9adaa352217054abe88a23
GitHub <noreply@github.com>
2024-02-08 01:35:25 +0300

    @hotwired/stimulus/e1cb435 docs: Fix action in "Multiple Actions" section (#749)
    https://github.com/hotwired/stimulus/tree/e1cb4359d29d32b660f6bd5b0801a3497b7a87a3

---
+++ dist/core/router.js
@@ -0,0 +1,122 @@
+import { Module } from "./module";
+import { Multimap } from "../multimap";
+import { Scope } from "./scope";
+import { ScopeObserver } from "./scope_observer";
+var Router = (function () {
+    function Router(application) {
+        this.application = application;
+        this.scopeObserver = new ScopeObserver(this.element, this.schema, this);
+        this.scopesByIdentifier = new Multimap();
+        this.modulesByIdentifier = new Map();
+    }
+    Object.defineProperty(Router.prototype, "element", {
+        get: function () {
+            return this.application.element;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Router.prototype, "schema", {
+        get: function () {
+            return this.application.schema;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Router.prototype, "logger", {
+        get: function () {
+            return this.application.logger;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Router.prototype, "controllerAttribute", {
+        get: function () {
+            return this.schema.controllerAttribute;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Router.prototype, "modules", {
+        get: function () {
+            return Array.from(this.modulesByIdentifier.values());
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Router.prototype, "contexts", {
+        get: function () {
+            return this.modules.reduce(function (contexts, module) { return contexts.concat(module.contexts); }, []);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Router.prototype.start = function () {
+        this.scopeObserver.start();
+    };
+    Router.prototype.stop = function () {
+        this.scopeObserver.stop();
+    };
+    Router.prototype.loadDefinition = function (definition) {
+        this.unloadIdentifier(definition.identifier);
+        var module = new Module(this.application, definition);
+        this.connectModule(module);
+        var afterLoad = definition.controllerConstructor.afterLoad;
+        if (afterLoad) {
+            afterLoad.call(definition.controllerConstructor, definition.identifier, this.application);
+        }
+    };
+    Router.prototype.unloadIdentifier = function (identifier) {
+        var module = this.modulesByIdentifier.get(identifier);
+        if (module) {
+            this.disconnectModule(module);
+        }
+    };
+    Router.prototype.getContextForElementAndIdentifier = function (element, identifier) {
+        var module = this.modulesByIdentifier.get(identifier);
+        if (module) {
+            return module.contexts.find(function (context) { return context.element == element; });
+        }
+    };
+    Router.prototype.proposeToConnectScopeForElementAndIdentifier = function (element, identifier) {
+        var scope = this.scopeObserver.parseValueForElementAndIdentifier(element, identifier);
+        if (scope) {
+            this.scopeObserver.elementMatchedValue(scope.element, scope);
+        }
+        else {
+            console.error("Couldn't find or create scope for identifier: \"".concat(identifier, "\" and element:"), element);
+        }
+    };
+    Router.prototype.handleError = function (error, message, detail) {
+        this.application.handleError(error, message, detail);
+    };
+    Router.prototype.createScopeForElementAndIdentifier = function (element, identifier) {
+        return new Scope(this.schema, element, identifier, this.logger);
+    };
+    Router.prototype.scopeConnected = function (scope) {
+        this.scopesByIdentifier.add(scope.identifier, scope);
+        var module = this.modulesByIdentifier.get(scope.identifier);
+        if (module) {
+            module.connectContextForScope(scope);
+        }
+    };
+    Router.prototype.scopeDisconnected = function (scope) {
+        this.scopesByIdentifier.delete(scope.identifier, scope);
+        var module = this.modulesByIdentifier.get(scope.identifier);
+        if (module) {
+            module.disconnectContextForScope(scope);
+        }
+    };
+    Router.prototype.connectModule = function (module) {
+        this.modulesByIdentifier.set(module.identifier, module);
+        var scopes = this.scopesByIdentifier.getValuesForKey(module.identifier);
+        scopes.forEach(function (scope) { return module.connectContextForScope(scope); });
+    };
+    Router.prototype.disconnectModule = function (module) {
+        this.modulesByIdentifier.delete(module.identifier);
+        var scopes = this.scopesByIdentifier.getValuesForKey(module.identifier);
+        scopes.forEach(function (scope) { return module.disconnectContextForScope(scope); });
+    };
+    return Router;
+}());
+export { Router };
