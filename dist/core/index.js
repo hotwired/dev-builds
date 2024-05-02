4fcccb58d10ec00289705a9465246baeae2e321f
GitHub <noreply@github.com>
2024-05-02 09:38:29 -0400

    @hotwired/stimulus/e6f1887 Bump follow-redirects from 1.15.5 to 1.15.6 (#757)
    https://github.com/hotwired/stimulus/tree/e6f1887c9ac91f3d83d8cde0b80c216d0dc3de6e

---
+++ dist/core/index.js
@@ -0,0 +1,4 @@
+export { Application } from "./application";
+export { Context } from "./context";
+export { Controller } from "./controller";
+export { defaultSchema } from "./schema";
---
+++ dist/mutation-observers/attribute_observer.js
@@ -0,0 +1,66 @@
+import { ElementObserver } from "./element_observer";
+var AttributeObserver = (function () {
+    function AttributeObserver(element, attributeName, delegate) {
+        this.attributeName = attributeName;
+        this.delegate = delegate;
+        this.elementObserver = new ElementObserver(element, this);
+    }
+    Object.defineProperty(AttributeObserver.prototype, "element", {
+        get: function () {
+            return this.elementObserver.element;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(AttributeObserver.prototype, "selector", {
+        get: function () {
+            return "[".concat(this.attributeName, "]");
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AttributeObserver.prototype.start = function () {
+        this.elementObserver.start();
+    };
+    AttributeObserver.prototype.pause = function (callback) {
+        this.elementObserver.pause(callback);
+    };
+    AttributeObserver.prototype.stop = function () {
+        this.elementObserver.stop();
+    };
+    AttributeObserver.prototype.refresh = function () {
+        this.elementObserver.refresh();
+    };
+    Object.defineProperty(AttributeObserver.prototype, "started", {
+        get: function () {
+            return this.elementObserver.started;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AttributeObserver.prototype.matchElement = function (element) {
+        return element.hasAttribute(this.attributeName);
+    };
+    AttributeObserver.prototype.matchElementsInTree = function (tree) {
+        var match = this.matchElement(tree) ? [tree] : [];
+        var matches = Array.from(tree.querySelectorAll(this.selector));
+        return match.concat(matches);
+    };
+    AttributeObserver.prototype.elementMatched = function (element) {
+        if (this.delegate.elementMatchedAttribute) {
+            this.delegate.elementMatchedAttribute(element, this.attributeName);
+        }
+    };
+    AttributeObserver.prototype.elementUnmatched = function (element) {
+        if (this.delegate.elementUnmatchedAttribute) {
+            this.delegate.elementUnmatchedAttribute(element, this.attributeName);
+        }
+    };
+    AttributeObserver.prototype.elementAttributeChanged = function (element, attributeName) {
+        if (this.delegate.elementAttributeValueChanged && this.attributeName == attributeName) {
+            this.delegate.elementAttributeValueChanged(element, attributeName);
+        }
+    };
+    return AttributeObserver;
+}());
+export { AttributeObserver };
